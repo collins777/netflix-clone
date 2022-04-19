98de808442a029c5a859765df92020040e041be3
Brandon Collins <brandoncollins@Brandons-MBP.lan>
2022-04-19 13:16:15 -0400

    initial commit with working project files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: "netflix-clone-ab4d1.firebaseapp.com",
+  projectId: "netflix-clone-ab4d1",
+  storageBucket: "netflix-clone-ab4d1.appspot.com",
+  messagingSenderId: "552037460625",
+  appId: "1:552037460625:web:70f347573784e941ed6d30",
+};
