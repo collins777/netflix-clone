5a66b87bf999a2477d64c5efa63b02ef3992bc34
Brandon Collins <brandoncollins@Brandons-MBP.lan>
2022-04-19 14:02:47 -0400

    finished nav component and styling

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 const Nav = () => {
-  return <div className="nav">Nav</div>;
+  const [show, handleShow] = useState(false);
+
+  // Navbar Scroll Effect
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  // Navbar Scroll Effect
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav--black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="Netflix Logo"
+        />
+        <img
+          className="nav__avatar"
+          src="https://external-preview.redd.it/0dTT-3SprPcsNCqo1GTCI-nqGM9EdZYwqyYr_pZ-baE.jpg?auto=webp&s=a1e8532d326f5aa122df2f31694bf142f117fc06"
+          alt="User Avatar"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
