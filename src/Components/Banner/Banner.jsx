e3801a4e7cb7cb11601d803d42633922952fb956
Brandon Collins <brandoncollins@Brandons-MBP.lan>
2022-04-23 13:53:27 -0400

    updated the banner to interact with tmdb

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "../../axios";
+import requests from "../../Requests";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   const truncate = (string, n) => {
     return string?.length > n ? string.substring(0, n - 1) + "..." : string;
   };
@@ -11,30 +31,23 @@ const Banner = () => {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC)`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, sit
-          eaque fugiat vero expedita sint distinctio veniam repudiandae ipsa
-          itaque? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Beatae, sit eaque fugiat vero expedita sint distinctio veniam
-          repudiandae ipsa itaque? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Beatae, sit eaque fugiat vero expedita sint
-          distinctio veniam repudiandae ipsa itaque?`,
-            150
-          )}
+          {truncate(movie?.overview, 170)}
         </h1>
       </div>
-      <div className="banner--fadeButtom"></div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 };
